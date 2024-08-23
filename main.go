b9406ed09d4b89996d9e2d8e8a485f7d0079ffc5
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-08-23 23:17:30 +0530

    added mongodb to the collection

--- main.go
+++ main.go
@@ -1,195 +1,171 @@
 package main
 
 import (
-	"errors"
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"net/http"
+	"time"
 )
 
 type movies struct {
-	Id           string `json:"id"`
-	Name         string `json:"name"`
-	Release_date string `json:"date"`
-	Director     string `json:"director"`
-	Stars        int    `json:"stars"`
+	ID           string `json:"id" bson:"_id,omitempty"`
+	Name         string `json:"name" bson:"name"`
+	ReleaseDate  string `json:"date" bson:"release_date"`
+	Director     string `json:"director" bson:"director"`
+	Stars        int    `json:"stars" bson:"stars"`
 }
 
-var film = []movies{
-	{
-		Id:           "42",
-		Name:         "The Infinite Horizon",
-		Release_date: "2023-07-16",
-		Director:     "Jane Doe",
-		Stars:        4,
-	},
-	{
-		Id:           "17",
-		Name:         "Beyond the Mist",
-		Release_date: "2021-03-22",
-		Director:     "John Smith",
-		Stars:        3,
-	},
-	{
-		Id:           "29",
-		Name:         "Echoes of Eternity",
-		Release_date: "2020-11-09",
-		Director:     "Alice Johnson",
-		Stars:        5,
-	},
-	{
-		Id:           "34",
-		Name:         "The Last Beacon",
-		Release_date: "2022-05-12",
-		Director:     "Robert Lee",
-		Stars:        4,
-	},
-	{
-		Id:           "58",
-		Name:         "Whispers in the Dark",
-		Release_date: "2021-08-19",
-		Director:     "Emily Carter",
-		Stars:        2,
-	},
-	{
-		Id:           "63",
-		Name:         "The Silent Voyager",
-		Release_date: "2019-02-14",
-		Director:     "Michael Brown",
-		Stars:        5,
-	},
-	{
-		Id:           "77",
-		Name:         "Lost in Time",
-		Release_date: "2024-01-01",
-		Director:     "Sophia Green",
-		Stars:        3,
-	},
-	{
-		Id:           "82",
-		Name:         "Shadows of Tomorrow",
-		Release_date: "2022-09-30",
-		Director:     "David Wilson",
-		Stars:        4,
-	},
-	{
-		Id:           "96",
-		Name:         "The Forgotten Path",
-		Release_date: "2023-06-07",
-		Director:     "Lucas Martinez",
-		Stars:        3,
-	},
-	{
-		Id:           "105",
-		Name:         "Rise of the Guardians",
-		Release_date: "2020-12-25",
-		Director:     "Olivia Clark",
-		Stars:        4,
-	},
+var movieCollection *mongo.Collection
+
+// Connect to MongoDB
+func connectToMongoDB() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
+
+	client, err := mongo.NewClient(clientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected to MongoDB!")
+	return client, nil
 }
 
 // Fetch all movies
 func GetAllMovies(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, film)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := movieCollection.Find(ctx, bson.M{})
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "could not retrieve movies"})
+		return
+	}
+
+	var movies []movies
+	if err = cursor.All(ctx, &movies); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "could not decode movies"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, movies)
 }
 
 // Fetch movie by ID
 func moviesByID(c *gin.Context) {
 	id := c.Param("id")
-	movie, err := GetMoviesById(id)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var movie movies
+	err := movieCollection.FindOne(ctx, bson.M{"_id": id}).Decode(&movie)
 	if err != nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "movie not found"})
 		return
 	}
+
 	c.IndentedJSON(http.StatusOK, movie)
 }
 
 // Fetch movie by name
 func moviesByName(c *gin.Context) {
 	name := c.Param("name")
-	movie, err := GetMoviesByName(name)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var movie movies
+	err := movieCollection.FindOne(ctx, bson.M{"name": name}).Decode(&movie)
 	if err != nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "movie not found"})
 		return
 	}
+
 	c.IndentedJSON(http.StatusOK, movie)
 }
 
-// Get movie by ID
-func GetMoviesById(id string) (*movies, error) {
-	for i, j := range film {
-		if j.Id == id {
-			return &film[i], nil
-		}
-	}
-	return nil, errors.New("movie not found")
-}
-
-// a extra function for deleting the movie
-func GetMoviesById_delete(id string) (int, error) {
-	for i, j := range film {
-		if j.Id == id {
-			return i, nil
-		}
-	}
-	return -1, errors.New("movie not found")
-}
-
-// Get movie by name
-func GetMoviesByName(name string) (*movies, error) {
-	for i, j := range film {
-		if j.Name == name {
-			return &film[i], nil
-		}
-	}
-	return nil, errors.New("movie not found")
-}
-
-// for deleting the movie by using their id
+// Delete movie by ID
 func deleteMovieByID(c *gin.Context) {
 	id := c.Param("id")
-	index, err := GetMoviesById_delete(id)
-	if err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := movieCollection.DeleteOne(ctx, bson.M{"_id": id})
+	if err != nil || result.DeletedCount == 0 {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "movie not found"})
 		return
 	}
-	film = append(film[:index], film[index+1:]...)
+
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "movie deleted"})
 }
 
-// creating a movie list
+// Create a movie
 func createMovie(c *gin.Context) {
 	var newMovie movies
 	if err := c.BindJSON(&newMovie); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "incorrect Json format"})
 		return
 	}
-	film = append(film, newMovie)
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "movie added succesfully"})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_, err := movieCollection.InsertOne(ctx, newMovie)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "could not create movie"})
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "movie added successfully"})
 }
 
-// updating the movies
+// Update a movie
 func UpdateMovie(c *gin.Context) {
-	//getting the id of the movie which i want to update
 	id := c.Param("id")
 	var update movies
 	if err := c.BindJSON(&update); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "can't be done in json/invaild json format"})
 		return
 	}
-	for i, m := range film {
-		if m.Id == id {
-			film[i] = update
-			c.IndentedJSON(http.StatusOK, gin.H{"message": "movie updated successfully"})
-			return
-		}
 
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	result, err := movieCollection.UpdateOne(ctx, bson.M{"_id": id}, bson.M{"$set": update})
+	if err != nil || result.ModifiedCount == 0 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "movie not found or could not be updated"})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "cannot found the movie please added one"})
 
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "movie updated successfully"})
 }
 
 // Define the routes for the handler
 func main() {
+	client, err := connectToMongoDB()
+	if err != nil {
+		panic(err)
+	}
+
+	// Update the database and collection names
+	movieCollection = client.Database("Movies").Collection("film")
+
 	router := gin.Default()
 	router.GET("/movies", GetAllMovies)
 	router.GET("/movies/id/:id", moviesByID)
